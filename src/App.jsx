84b60a59911ab6959289f1885a89aca23fddfe6b
marufhasan34 <YOUR_GITHUB_EMAIL>
2026-03-30 23:58:28 +0600

    counter section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Banner from './Components/Banner/Banner'
+import Counter from './Components/Counter/Counter'
 import Navbar from './Components/Navbar/Navbar'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
      <Navbar/>
      <Banner/>
+     <Counter/>
     </>
   )
 }
